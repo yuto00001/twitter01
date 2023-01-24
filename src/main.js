0d1294b3dd20f11dad2b40dd0ec191c526510099
YUTO TANISHIKI <yutotanishiki@TANISHIKI.elecom>
2023-01-24 14:35:02 +0900

    commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import firebase from "firebase/app"
+import "firebase/auth";
+import {firebaseConfig} from '@/config/local.js'
 
 Vue.config.productionTip = false
+firebase.initializeApp(firebaseConfig)
+
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
